cb355e671d1bec757d34b11c00608388dba105ce
zhuzhile <2364180085@qq.com>
2021-03-23 23:50:02 +0800

    feature: 使用promise实现ajax

---
+++ test-07.js
@@ -0,0 +1,29 @@
+var url = 'http://jsonplaceholder.typicode.com/albums/6';
+
+let getData = function (){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = handler;
+
+        xhr.open('GET', url);
+        xhr.send();
+
+         function handler(){
+            console.log('hello');
+            if( xhr.readyState === 4 ){
+                if(xhr.status === 200 ){
+                     resolve(xhr.responseText);
+                }else{
+                    reject(new Error("fucking error"));
+                }
+            }
+        }
+    })
+}
+
+getData().then(function(success){
+    console.log(success);
+}, function(fail){
+    console.error(fail)
+})
